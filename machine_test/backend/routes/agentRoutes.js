73a831dc78bda9863b2518fc663b3889d1688929
Sarbottam Abhijeet <abhijeetsarbottam@gmail.com>
2026-01-29 17:02:09 +0530

    Restore missing getLeads function and finalize project

--- machine_test/backend/routes/agentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { addAgent, getAgents } = require('../controllers/agentController');
-const { protect } = require('../middlewares/authMiddleware');
-const router = express.Router();
-
-router.route('/').post(protect, addAgent).get(protect, getAgents);
-
-module.exports = router;
--- machine_test/backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { authUser, registerUser } = require('../controllers/userController');
-const router = express.Router();
-
-router.post('/login', authUser);
-router.post('/register', registerUser);
-
-module.exports = router;
